77835f1e941f18daece638f0c17b8b4dabee5733
CSE1-19-LE-74 <104007876+CSE1-19-LE-74@users.noreply.github.com>
2022-07-29 18:23:39 +0530

    Create twoarrays.java

---
+++ twoarrays.java
@@ -0,0 +1,15 @@
+public class ArrayEqualsExample1  
+{   
+public static void main (String[] args)    
+{   
+//defining arrays to be compare   
+int[] a1 = new int[] {1, 2, 3, 4, 5, 6, 7, 8};   
+int[] a2 = new int[] {1, 2, 3, 4, 5, 6, 7, 8};   
+//comparing references using == operator  
+//works the same as a1.equals(a2)   
+if (a1 == a2)   
+System.out.println("Arrays are equal.");   
+else  
+System.out.println("Arrays are not equal.");   
+}   
+}  
